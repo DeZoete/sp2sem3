e46739fe214b175121709ffc6ecf8f62dd5260db
Marcus-Ekeroth <cph-me313@cphbusiness.dk>
2024-10-23 12:51:09 +0200

    speciescontroller

--- src/main/java/dat/controllers/impl/SpeciesController.java
+++ src/main/java/dat/controllers/impl/SpeciesController.java
@@ -1,33 +1,55 @@
 package dat.controllers.impl;
 
 import dat.controllers.IController;
+import dat.daos.SpeciesDAO;
 import dat.dtos.SpeciesDTO;
 import io.javalin.http.Context;
 
+import java.util.List;
+
 public class SpeciesController implements IController<SpeciesDTO, Integer>{
+
+    private SpeciesDAO speciesDAO;
     @Override
     public void read(Context ctx) {
-
+        {
+            int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+            SpeciesDTO speciesDTO = speciesDAO.read(id);
+            ctx.res().setStatus(200);
+            ctx.json(speciesDTO, SpeciesDTO.class);
+        }
     }
 
     @Override
     public void readAll(Context ctx) {
-
+    List<SpeciesDTO> speciesDTOS = speciesDAO.readAll();
+    ctx.res().setStatus(200);
+    ctx.json(speciesDTOS, SpeciesDTO.class);
     }
 
     @Override
     public void create(Context ctx) {
-
+        SpeciesDTO jsonRequest = ctx.bodyAsClass(SpeciesDTO.class);
+        // DTO
+        SpeciesDTO speciesDTO = speciesDAO.create(jsonRequest);
+        // response
+        ctx.res().setStatus(201);
+        ctx.json(speciesDTO, SpeciesDTO.class);
     }
 
     @Override
     public void update(Context ctx) {
-
+        int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+        SpeciesDTO speciesDTO = speciesDAO.update(id, validateEntity(ctx));
+        ctx.res().setStatus(200);
+        ctx.json(speciesDTO, SpeciesDTO.class);
     }
 
     @Override
     public void delete(Context ctx) {
-
+        int id = ctx.pathParamAsClass("id", Integer.class).check(this::validatePrimaryKey, "Not a valid id").get();
+        speciesDAO.delete(id);
+        ctx.res().setStatus(204);
     }
 
     @Override
