1fc048f24a953d21c10f49c6fe86d0d40bac96e0
FrederikMoestrup <cph-fm124@cphbusiness.dk>
2024-10-24 17:23:20 +0200

    Jeg kan ikke få populate til at virke

--- src/main/java/dat/config/Populate.java
+++ src/main/java/dat/config/Populate.java
@@ -6,22 +6,9 @@ import dat.entities.Zoo;
 import jakarta.persistence.EntityManagerFactory;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Populate {
-    /*
-    private static List<Animal> zoo1Animals = getZoo1Animals().stream().collect(Collectors.toList());
-    private static List<Animal> zoo2Animals = getZoo2Animals().stream().collect(Collectors.toList());
-
-    private static Zoo zoo1 = new Zoo("Zoo 1", "Location 1");
-    private static Zoo zoo2 = new Zoo("Zoo 2", "Location 2");
-   private static Species lion = new Species("Lion", "Carnivore", "Savannah");
-   private static Species tiger = new Species("Tiger", "Carnivore", "Forest");
-   private static Species elephant = new Species("Elephant", "Herbivore", "Savannah");
-   private static Species giraffe = new Species("Giraffe", "Herbivore", "Savannah");
-   private static Species zebra = new Species("Zebra", "Herbivore", "Savannah");
 
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("animal");
@@ -29,53 +16,68 @@ public class Populate {
         try (var em = emf.createEntityManager()) {
             em.getTransaction().begin();
 
-            // persist zoos
+            // Create species
+            Species lion = new Species("Lion", "Carnivore", "Savannah");
+            Species tiger = new Species("Tiger", "Carnivore", "Forest");
+            Species elephant = new Species("Elephant", "Herbivore", "Savannah");
+            Species giraffe = new Species("Giraffe", "Herbivore", "Savannah");
+            Species zebra = new Species("Zebra", "Herbivore", "Savannah");
+
+            // Create zoos and animals
+            Set<Animal> zoo1Animals = getZoo1Animals(lion, tiger, elephant, giraffe, zebra);
+            Set<Animal> zoo2Animals = getZoo2Animals(tiger, elephant, giraffe, zebra);
+
+            Zoo zoo1 = new Zoo("Odense Zoo", "Fyn et elle andet sted");
+            Zoo zoo2 = new Zoo("Zoologisk have Lyngby", "Lyngby Storcenter");
+            zoo1.setAnimals(zoo1Animals);
+            zoo2.setAnimals(zoo2Animals);
+
+            // Persist zoos
             em.persist(zoo1);
             em.persist(zoo2);
-            // persist species
+
+            // Persist species
             em.persist(lion);
             em.persist(tiger);
             em.persist(elephant);
             em.persist(giraffe);
             em.persist(zebra);
 
-
-            zoo1.setAnimals(zoo1Animals);
-            zoo2.setAnimals(zoo2Animals);
-
-            zoo1Animals.forEach(em::persist);
-            zoo2Animals.forEach(em::persist);
-
             em.getTransaction().commit();
         }
     }
 
     @NotNull
-    private static Set<Animal> getZoo1Animals() {
+    private static Set<Animal> getZoo1Animals(Species lion, Species tiger, Species elephant, Species giraffe, Species zebra) {
+        // Set species for each animal
+        Animal a1 = new Animal("Simba", 5);
+        a1.setSpecies(lion);
+        Animal a2 = new Animal("Shere Khan", 3);
+        a2.setSpecies(tiger);
+        Animal a3 = new Animal("Dumbo", 10);
+        a3.setSpecies(elephant);
+        Animal a4 = new Animal("Melman", 7);
+        a4.setSpecies(giraffe);
+        Animal a5 = new Animal("Marty", 4);
+        a5.setSpecies(zebra);
+        Animal a6 = new Animal("Nala", 7);
+        a6.setSpecies(lion);
 
-
-        Animal a1 = new Animal(lion.getId(), zoo1, "Simba", 5);
-        Animal a2 = new Animal(tiger.getId(), zoo1, "Shere Khan", 3);
-        Animal a3 = new Animal(elephant.getId(), zoo1, "Dumbo", 10);
-        Animal a4 = new Animal(giraffe.getId(), zoo1, "Melman", 7);
-        Animal a5 = new Animal(zebra.getId(), zoo1, "Marty", 4);
-
-        Animal[] animalArray = {a1, a2, a3, a4, a5};
-        return Set.of(animalArray);
+        return Set.of(a1, a2, a3, a4, a5, a6);
     }
 
     @NotNull
-    private static Set<Animal> getZoo2Animals() {
+    private static Set<Animal> getZoo2Animals(Species tiger, Species elephant, Species giraffe, Species zebra) {
+        // Set species for each animal
+        Animal a7 = new Animal("Rajah", 4);
+        a7.setSpecies(tiger);
+        Animal a8 = new Animal("Babar", 2);
+        a8.setSpecies(elephant);
+        Animal a9 = new Animal("Geoffrey", 6);
+        a9.setSpecies(giraffe);
+        Animal a10 = new Animal("Stripes", 3);
+        a10.setSpecies(zebra);
 
-        Animal a6 = new Animal(lion.getId(), zoo2, "Nala", 7);
-        Animal a7 = new Animal(tiger.getId(), zoo2, "Rajah", 4);
-        Animal a8 = new Animal(elephant.getId(), zoo2, "Babar", 2);
-        Animal a9 = new Animal(giraffe.getId(), zoo2, "Geoffrey", 6);
-        Animal a10 = new Animal(zebra.getId(), zoo2, "Stripes", 3);
-
-        Animal[] animalArray = {a6, a7, a8, a9, a10};
-        return Set.of(animalArray);
+        return Set.of(a7, a8, a9, a10);
     }
-    */
-
 }
